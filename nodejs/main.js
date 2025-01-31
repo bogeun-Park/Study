e979cd3d698a9ab76fe8054f212331f1b97e6130
bogeun-Park <qhrms0528@gmail.com>
2025-01-31 17:27:24 +0900

    20250131 1차

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');  // 보안문제 예방
